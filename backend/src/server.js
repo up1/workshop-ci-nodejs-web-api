81fd0e9fdfb8a1bc72423610452ca36b975f4543
up1 <somkiat.p@gmail.com>
2023-06-14 14:33:50 +0700

    Try to pass

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ const metricsMiddleware = promBundle({includeMethod: true});
 app.use(metricsMiddleware);
 
 app.get("/", function (req, res) {
-    res.json({ message: 'Hello World...' })
+    res.json({ message: 'Hello World' })
 });
 
 app.get("/hello", function (req, res) {
