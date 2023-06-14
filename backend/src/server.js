4866cd90e0a27e3d81819ea0c9d401fa082d0282
up1 <somkiat.p@gmail.com>
2023-06-14 14:17:17 +0700

    Try to fail

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,11 @@ const metricsMiddleware = promBundle({includeMethod: true});
 app.use(metricsMiddleware);
 
 app.get("/", function (req, res) {
-    res.json({ message: 'Hello World' })
+    res.json({ message: 'Hello World...' })
+});
+
+app.get("/hello", function (req, res) {
+    res.json({ message: 'Hello World...' })
 });
 
 module.exports = app;
