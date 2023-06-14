d58e475dba07d04576fb7421a5ed1c8908c649bd
up1 <somkiat.p@gmail.com>
2023-06-14 15:13:47 +0700

    Try to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ const metricsMiddleware = promBundle({includeMethod: true});
 app.use(metricsMiddleware);
 
 app.get("/", function (req, res) {
-    res.json({ message: 'Hello World' })
+    res.json({ message: 'Hello World with deployment' })
 });
 
 app.get("/hello", function (req, res) {
