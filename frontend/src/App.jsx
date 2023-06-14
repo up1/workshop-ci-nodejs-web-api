db2a0d40d2e19e4b5a0c15def54d7ee7c9138612
up1 <somkiat.p@gmail.com>
2023-06-14 12:57:29 +0700

    Add miragejs to mock api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import { createServer } from "miragejs"
+
+createServer({
+  routes() {
+    this.get("https://jsonplaceholder.typicode.com/users", () => [
+      { id: "1", name: "User 01" },
+      { id: "2", name: "User 02" },
+      { id: "3", name: "User 03" },
+    ])
+  },
+})
+
 function App() {
   const [users, setUsers] = useState([])
 
   useEffect(() => {
     axios.get('https://jsonplaceholder.typicode.com/users')
+    // axios.get('/users')
       .then(response => {
         setUsers(response.data)
       })
