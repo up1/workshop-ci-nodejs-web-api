2fc48f5888dc0bb30cce2cfdc974df19f6b75e47
up1 <somkiat.p@gmail.com>
2023-06-15 13:03:47 +0700

    remove mock server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import { createServer } from "miragejs"
+// import { createServer } from "miragejs"
 
-createServer({
-  routes() {
-    this.get("https://jsonplaceholder.typicode.com/users", () => [
-      { id: "1", name: "User 01" },
-      { id: "2", name: "User 02" },
-      { id: "3", name: "User 03" },
-    ])
-  },
-})
+// createServer({
+//   routes() {
+//     this.get("https://jsonplaceholder.typicode.com/users", () => [
+//       { id: "1", name: "User 01" },
+//       { id: "2", name: "User 02" },
+//       { id: "3", name: "User 03" },
+//     ])
+//   },
+// })
 
 function App() {
   const [users, setUsers] = useState([])
