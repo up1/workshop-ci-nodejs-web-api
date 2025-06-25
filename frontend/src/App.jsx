f0731d795b8e91c57011481917cf685ce445fb8d
up1 <somkiat.p@gmail.com>
2025-06-25 12:58:46 +0700

    Update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,11 +28,14 @@ function App() {
   }, [])
 
   return (
-    <ul>
-      {users.map(user => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <div>
+      <h2>Users List</h2>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
