6caaf30d6a99392745f7b1f9048ae2fb4dae3c93
up1 <somkiat.p@gmail.com>
2025-06-25 15:20:21 +0700

    Add more tests

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ const promBundle = require("express-prom-bundle");
 const express = require("express");
 const app = express();
 
+
+
 const metricsMiddleware = promBundle({includeMethod: true});
 app.use(metricsMiddleware);
 
